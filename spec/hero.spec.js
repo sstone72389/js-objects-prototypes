347eec6b6a06e07424f2d8d6c7910609ce127a0b
Toni Langley <chibia2@gmail.com>
2017-03-17 15:03:25 -0400

    Update from talk template and node template
    Update to standard js style
    Refresh for lm01

--- spec/hero.spec.js
+++ spec/hero.spec.js
@@ -1,21 +1,17 @@
-'use strict';
+'use strict'
 
-const chai = require('chai');
-const expect = chai.expect;
+const chai = require('chai')
+const expect = chai.expect
 
-const Hero = require('../lib/hero');
+const Hero = require('../lib/hero')
 
 describe('Storm', function () {
-
-  let storm = new Hero('Ororo Iquadi T\'Challa',
-    'Storm', 'weather control');
+  const storm = new Hero('Ororo Iquadi T\'Challa',
+    'Storm', 'weather control')
 
   xdescribe('super power', function () {
-
     it('weather control', function () {
-      expect(storm._power).to.equal('weather control');
-    });
-
-  });
-
-});
+      expect(storm._power).to.equal('weather control')
+    })
+  })
+})
--- spec/runs.spec.js
+++ spec/runs.spec.js
@@ -1,51 +1,43 @@
-'use strict';
+'use strict'
 
-const chai = require('chai');
-const expect = chai.expect;
+const chai = require('chai')
+const expect = chai.expect
 
-const runs = require('../lib/runs');
+const runs = require('../lib/runs')
 
 describe('runs', function () {
-
-  const user = new runs.User('Person McFace', 'wdi@personmcface.com');
-  user.runs.push(new runs.Run('2015-05-25 15:00', 1200, 600));
-  user.runs.push(new runs.Run('2015-05-25 15:00', 1400, 800));
+  const user = new runs.User('Person McFace', 'wdi@personmcface.com')
+  user.runs.push(new runs.Run('2015-05-25 15:00', 1200, 600))
+  user.runs.push(new runs.Run('2015-05-25 15:00', 1400, 800))
 
   describe('user', function () {
     it('has the correct name and e-mail address', function () {
-      expect(user.name).to.equal('Person McFace');
-      expect(user.email).to.equal('wdi@personmcface.com');
-    });
-  });
+      expect(user.name).to.equal('Person McFace')
+      expect(user.email).to.equal('wdi@personmcface.com')
+    })
+  })
 
   describe('length', function () {
     it('is correct', function () {
-      expect(user.runs.length).to.equal(2);
-    });
-  });
+      expect(user.runs.length).to.equal(2)
+    })
+  })
 
   xdescribe('total distance', function () {
-
     it('has the correct length', function () {
-      expect(runs.totalDistance()).to.equal(2600);
-    });
-
-  });
+      expect(runs.totalDistance()).to.equal(2600)
+    })
+  })
 
   xdescribe('longest run', function () {
-
     it('has the correct length', function () {
-      expect(runs.longestRun()).to.equal(1400);
-    });
-
-  });
+      expect(runs.longestRun()).to.equal(1400)
+    })
+  })
 
   xdescribe('average speed', function () {
-
     it('has the correct value', function () {
-      expect(runs.averageSpeed().toLocaleString('en-US')).to.equal('1.857');
-    });
-
-  });
-
-});
+      expect(runs.averageSpeed().toLocaleString('en-US')).to.equal('1.857')
+    })
+  })
+})
